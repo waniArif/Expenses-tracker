2f0ffa8b9195d6676b0ba900b9296f1028c4e391
Arif <waniarifrasool@gmail.com>
2021-06-07 21:19:17 +0530

    init commit, added some components, basic look without state

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./expenseDate.css";
+
+const ExpenseDate = ({ date }) => {
+  return (
+    <div className="expense-date">
+      <div className=".expense-date__month">
+        {date.toLocaleString("en-US", { month: "long" })}
+      </div>
+      <div className="expense-date__day">
+        {date.toLocaleString("en-US", { day: "2-digit" })}
+      </div>
+      <div className="expense-date__year">{date.getFullYear()}</div>
+    </div>
+  );
+};
+
+export default ExpenseDate;
